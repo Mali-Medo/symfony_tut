9fbaaae3b198bf3e38db6033e135bd8315f0ee7e
Bart <bartolic1996@gmail.com>
2021-05-10 12:22:59 +0200

    After tut ch2 KnpMarkdownBundle

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -30,18 +31,20 @@ class BlogController extends AbstractController
      * @param $slug
      * @return Response
      */
-    public  function show($slug): Response
+    public  function show($slug, MarkdownParserInterface $markdownParser): Response
     {
         $comments = [
-          'First comment',
+          'First `comment`',
           'second comment',
           'third comment',
         ];
 
-        dump($this);
+        $blogText = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
+        $parsedBlogText = $markdownParser->transformMarkdown($blogText);
 
         return $this->render('blog/show.html.twig', [
             'blog' => ucwords(str_replace('-', ' ', $slug)),
+            'blogText' => $parsedBlogText,
             'comments' => $comments,
         ]);
 
