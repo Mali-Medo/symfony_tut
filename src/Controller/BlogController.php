243d5448c6c4feb6f11f323c75a662be5e130dce
Bart <bartolic1996@gmail.com>
2021-05-13 20:07:12 +0200

    before zenstruck

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -47,37 +47,7 @@ class BlogController extends AbstractController
      */
     public function new(EntityManagerInterface $entityManager): Response
     {
-        $blog = new Blog();
-        $blog->setTitle('Missing pants')
-            ->setSlug('missing-pants-'.rand(0, 1000))
-            ->setDescription('Does anyone have a spell to call your pants back?')
-            ->setBlogText(<<<EOF
-Hi! So... I'm having a *weird* day. Yesterday, I cast a spell to make
-my dishes wash themselves. But while I was casting it,
-I slipped a little and I think `I also hit my pants with the spell`.
-
-When I woke up this morning, I caught a quick glimpse of my pants
-opening the front door and walking out! I've been out all afternoon
-(with no pants mind you) searching for them.
-
-Does anyone have a spell to call your pants back?
-EOF
-            );
-
-        if(rand(1, 10) > 2){
-            $blog->setPostedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
-        }
-
-        $blog->setVotes(rand(-20, 50));
-
-        $entityManager->persist($blog);
-        $entityManager->flush();
-
-        return new Response(sprintf(
-            'Well hello! The shiny new question is id #%d, slug %s',
-            $blog->getId(),
-            $blog->getSlug(),
-        ));
+        return new Response('Spunds like a great feature for v2');
     }
 
     /**
